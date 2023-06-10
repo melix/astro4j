d7ba3547c4072f756120e9cf724eaed2dac94dfb
Cedric Champeau <cedric.champeau@gmail.com>
2023-06-10 14:44:49 +0200

    Do not change version for Windows/Mac anymore

--- build-logic/src/main/kotlin/me.champeau.astro4j.app.gradle.kts
+++ build-logic/src/main/kotlin/me.champeau.astro4j.app.gradle.kts
@@ -1,14 +1,11 @@
-import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.compile.JavaCompile
 import org.gradle.api.tasks.testing.Test
-import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
 import org.javamodularity.moduleplugin.extensions.TestModuleOptions
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-import java.util.*
 
 plugins {
     id("me.champeau.astro4j.base")
@@ -20,10 +17,6 @@ plugins {
 val date = LocalDateTime.now()
     .atZone(ZoneId.of("UTC"))
     .format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"))
-val os = System.getProperty("os.name").lowercase(Locale.ENGLISH)
-if (os.startsWith("windows") || os.contains("mac")) {
-    version = version.toString().substring(0, version.toString().lastIndexOf(".")) + ".0"
-}
 
 // We can safely enable preview features because it's
 // an application, so no consumers except for the final
