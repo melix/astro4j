eeb5bd2659dadb4a6f2760159554ff99c8a5fd3a
Cedric Champeau <cedric.champeau@gmail.com>
2024-04-28 23:41:37 +0200

    Add download links to documentation

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -4,6 +4,16 @@ plugins {
 
 tasks {
     asciidoctor {
+        val downloadVersion = if (version.toString().contains("-SNAPSHOT")) {
+            version.toString().substringBefore("-SNAPSHOT")
+        } else {
+            version.toString()
+        }
+        val prefixName = if (version.toString().contains("-SNAPSHOT")) {
+            "jsolex-devel"
+        } else {
+            "jsolex"
+        }
         attributes(mapOf(
             "reproducible" to "",
             "nofooter" to "",
@@ -12,6 +22,8 @@ tasks {
             "source-highlighter" to "highlight.js",
             "highlightjs-theme" to "equilibrium-light",
             "highlightjsdir" to "highlight",
+            "version" to downloadVersion,
+            "prefixName" to prefixName
         ))
         resources(delegateClosureOf<CopySpec> {
             from("src/docs/asciidoc") {
