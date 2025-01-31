46b9420a56649d2e22cdb510296b01eab4fd2f67
Cedric Champeau <cedric.champeau@gmail.com>
2025-01-31 14:06:51 +0100

    Remove native arch flag

--- jsolex-scripting/build.gradle.kts
+++ jsolex-scripting/build.gradle.kts
@@ -31,7 +31,6 @@ graalvmNative {
         // Vector API is not supported in native-image yet
 //        buildArgs.addAll(listOf("--add-modules", "jdk.incubator.vector"))
         buildArgs.add("--gc=G1")
-        buildArgs.add("-march=native")
     }
 }
 
