775929e844d75c0eecc5c2d61f306d10fcfbe2c0
Cedric Champeau <cedric.champeau@gmail.com>
2025-04-14 08:06:56 +0200

    Make it possible to finish measurement by pressing ENTER

--- jsolex/src/main/java/me/champeau/a4j/jsolex/app/jfx/DistanceMeasurementPane.java
+++ jsolex/src/main/java/me/champeau/a4j/jsolex/app/jfx/DistanceMeasurementPane.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2023-2023 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package me.champeau.a4j.jsolex.app.jfx;
 
 import javafx.beans.binding.Bindings;
@@ -28,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
@@ -113,6 +99,8 @@ public class DistanceMeasurementPane extends BorderPane {
         setTop(topBar);
         HBox.setHgrow(topBar, javafx.scene.layout.Priority.ALWAYS);
         setupMouseHandlers();
+        setupKeyHandlers();
+        setFocusTraversable(true);
         this.radius = (ellipse.semiAxis().a() + ellipse.semiAxis().b()) / 2;
         this.cx = ellipse.center().a();
         this.cy = ellipse.center().b();
@@ -246,7 +234,7 @@ public class DistanceMeasurementPane extends BorderPane {
         path.setStroke(Color.RED);
         path.setStrokeWidth(2);
         if (imagePoints.isEmpty()) return path;
-        var first = imageToPane(imagePoints.getFirst());
+        var first = imageToPane(imagePoints.get(0));
         path.getElements().add(new MoveTo(first.getX(), first.getY()));
         for (int i = 1; i < imagePoints.size(); i++) {
             var p = imageToPane(imagePoints.get(i));
@@ -300,6 +288,18 @@ public class DistanceMeasurementPane extends BorderPane {
     private void setupMouseHandlers() {
         setOnMouseClicked(this::handleMouseClick);
         setOnMouseMoved(this::handleMouseMoved);
+    }
+
+    private void setupKeyHandlers() {
+        setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER || event.getCode() == KeyCode.ESCAPE) {
+                if (!currentMeasurement.fixedPoints.isEmpty() && currentMeasurement.fixedPoints.size() > 1) {
+                    recordMeasurement();
+                    finishDrawing();
+                }
+                event.consume();
+            }
+        });
     }
 
     private void handleMouseClick(MouseEvent event) {
