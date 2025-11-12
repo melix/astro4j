2617db0fa39a8802504820ed7cea03581dfc9f83
Cedric Champeau <cedric.champeau@gmail.com>
2025-11-12 18:31:55 +0100

    Remove reduction of conversion procs

--- jsolex-core/src/main/java/me/champeau/a4j/jsolex/processing/sun/SolexVideoProcessor.java
+++ jsolex-core/src/main/java/me/champeau/a4j/jsolex/processing/sun/SolexVideoProcessor.java
@@ -1461,9 +1461,6 @@ public class SolexVideoProcessor implements Broadcaster {
         var detectPhenomena = detectBorders || detectRedshifts || detectEllermanBombs || detectActiveRegions;
         var ops = new TaskCoordinator();
         int conversionProcs = Runtime.getRuntime().availableProcessors();
-        if (detectPhenomena) {
-            conversionProcs /= 2;
-        }
         try (var detectionPool = Executors.newFixedThreadPool(Math.max(2, Runtime.getRuntime().availableProcessors() / 2))) {
             try (var conversionPool = Executors.newFixedThreadPool(Math.max(2, conversionProcs))) {
                 var reconstructedImages = new float[images.length][totalLines][width];
