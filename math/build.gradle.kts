60816beda71a5ac63b26c571a20c3dcc739c09ba
Cedric Champeau <cedric.champeau@gmail.com>
2025-12-14 15:17:19 +0100

    Fix arm64 build

--- math/build.gradle.kts
+++ math/build.gradle.kts
@@ -9,7 +9,7 @@ description = "Provides some mathematical functions like linear or elliptic regr
 
 val lwjglNatives = when {
     org.gradle.internal.os.OperatingSystem.current().isWindows -> "natives-windows"
-    org.gradle.internal.os.OperatingSystem.current().isMacOsX -> "natives-macos"
+    org.gradle.internal.os.OperatingSystem.current().isMacOsX -> if (System.getProperty("os.arch") == "aarch64") "natives-macos-arm64" else "natives-macos"
     else -> "natives-linux"
 }
 
